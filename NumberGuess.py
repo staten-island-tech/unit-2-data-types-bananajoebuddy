474f75cd362ab979567855b553ef3a2c3214d4cb
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2023-10-11 13:59:31 -0400

    commit

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,9 +1,19 @@
 import random
-
-
-x = input("Guess a number: ")
-ans = random.choice(1,20)
-while(x!=ans):
-    if x == ans:
-        print ("correct")
-print(ans)
+correct = "false"
+guess = 0
+num = random.randint(1,10)
+while correct == "false":
+    guess = int(input("Guess a number: "))
+    history = list()
+    if guess == num:
+        print("Correct!")
+        correct = "true"
+    elif guess > num:
+        print("Lower")
+        history.append(guess)
+    elif guess < num:
+        print("Higher")
+        history.append(guess)
+    else:
+        print("huh")
+print(history)
